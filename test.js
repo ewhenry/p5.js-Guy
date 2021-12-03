8bdfb953465b95906eed74a8e793bac9cc72b2fc
henry <77937076+ewhenry@users.noreply.github.com>
2021-12-03 21:00:42 +0000

    EVerything

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 function setup() {
-  createCanvas(650,500);
-  rectMode(CENTER);
+  createCanvas(650,500)
+  fill(255)
+  frameRate(30)
+  rectMode(CENTER)
+  createLoop({duration:3, gif:true})
 }
 
 function keyPressed() {
@@ -11,5 +14,10 @@ function keyReleased() {
 
 
 function draw() {
-  background(0);
+  background(0)
+  translate(width / 2, height / 2)
+  const radius = height / 3
+  const x = cos(animLoop.theta) * radius
+  const y = sin(animLoop.theta) * radius
+  ellipse(x, y, 50, 50)
 }
