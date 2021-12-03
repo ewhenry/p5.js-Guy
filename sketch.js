e8cc15c4d5a82c341de7565e5136ebc227591c14
henry <77937076+ewhenry@users.noreply.github.com>
2021-12-03 21:06:21 +0000

    renamed files and adding gifs.

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var gif_loadImg, gif_createImg;
+
+function preload() {
+  gif_loadImg = loadImage("background.gif");
+  gif_createImg = createImg("background.gif");
+}
+
+function setup() {
+  background(0);
+  createCanvas(650,500);
+  frameRate(30);
+  rectMode(CENTER);
+  createLoop({duration:3, gif:true});
+}
+
+function keyPressed() {
+}
+
+function keyReleased() {
+}
+
+
+function draw() {
+  image(gif_loadImg, 50, 50);
+  gif_createImg.position(50, 350);
+}
