939740d691f5472a4544e9fcef518daeec315316
henry <77937076+ewhenry@users.noreply.github.com>
2021-12-06 09:47:45 -0500

    Added more functionality
     but circle won't increase with constrain properly.

--- sketch.js
+++ sketch.js
@@ -1,40 +1,58 @@
-let man;
 var gif_LoadImg;
+let fr = 60;
 
 function preload() {
   gif_LoadImg = loadImage("assets/background.gif");
 }
 
-function setup() {
-  createCanvas(650,500);
-  frameRate(60);
-  ellipseMode(CENTER);
-  tint(0, 153, 204);
-
-  man = new Guy();
+function DeBug() {
+  push();
+  fill(255);
+  textSize(25);
+  text(int(getFrameRate()), width - 50, 50);
+  text(man.sizeC, width/2, 100);
+  pop();
 }
 
 class Guy {
   constructor() {
-    this.size = 100;
     this.x = width/2;
     this.y = height/2;
+
+    this.minSize = 100;
+    this.maxSize = 150;
+    
+    this.size = 100;
+    this.sizeC = constrain(this.size, this.minSize, this.maxSize);
   }
   render() {
-    ellipse(this.x, this.y, this.size, this.size);
+    ellipse(this.x, this.y, this.sizeC, this.sizeC);
   }
+
+  interact() {
+      if (mouseIsPressed == true) {
+        this.size += 5;
+      }
+      else if (mouseIsPressed == false) {
+        this.size -= 2;
+      }
+    }
 }
 
-function mousePressed() {
-  if (Guy.x === height/2) {
-    Guy.x = height/2 + 20;
-  } else {
-    Guy.x = height/2;
-  }
+function setup() {
+  createCanvas(650,500);
+  frameRate(fr);
+  ellipseMode(CENTER);
+  tint(0, 153, 204);
+  man = new Guy();
 }
 
 function draw() {
   background(0);
-  image(gif_LoadImg, 0, 0, width, height);
+  //image(gif_LoadImg, 0, 0, width, height);
+
+  DeBug();
+
   man.render();
+  man.interact();
 }
